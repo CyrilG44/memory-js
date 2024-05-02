ea99d032072cfdba65af6888c0584244c59e9d5c
CyrilG44 <cyril.grignoux@laposte.net>
2024-05-02 14:24:58 +0200

    version 3

--- js/app.js
+++ js/app.js
@@ -5,47 +5,76 @@ let nbRounds=0;
 const prefix="ressources/";
 
 //to update later with Math.random(n) puis Math.round pour tirer la position au hasard
-const targetImages = [1,2,3,4,5,6,6,5,4,3,2,1];
-//const targetImages = new Array(2*nbCartes);
+//bug taille image lorsque choisie au hasard à résoudre + tard
+const targetImages = new Map(); // position -> img
+const cardStatuses = new Map();
+for (let i=1; i<=nbDistinctCards; i++) {
+    let nb=0;
+    let inc=0;
+    do {
+        let position = Math.ceil(Math.random()*(nbDistinctCards*2)); //arrondi au supérieur
+        if (targetImages.get("card"+position) == undefined) {
+            targetImages.set("card"+position,i);
+            cardStatuses.set("card"+position,0);
+            nb++;
+        }
+        inc++;
+    } while (nb<2);
+}
+console.log(targetImages);
 
-const cardStatuses = [0,0,0,0,0,0,0,0,0,0,0,0]
-//console.log(cardStatuses.length);
-
-//Set target images
-for (let i=0; i<nbDistinctCards*2; i++) {
-    document.getElementById("img"+(i+1)).src=prefix+targetImages[i]+".svg"
+//Set target images source
+for (let i=1; i<=nbDistinctCards*2; i++) {
+    document.getElementById("card"+i+"-img").src=prefix+targetImages.get("card"+i)+".svg";
+    //document.getElementById("img"+i).src=prefix+[1,2,3,4,5,6,6,5,4,3,2,1][i-1]+".svg"
 }
 
-//to optimize later (by for?)
-const $position1 = document.getElementById("position1");
-const $position2 = document.getElementById("position2");
-const $position3 = document.getElementById("position3");
+//stockage des éléments carte
+const cards = document.querySelectorAll(".card");
+
+//création des écoutes en masse
+cards.forEach(element => {
+    element.addEventListener('click', process);
+});
+
+//initialisation d'un tour
+let turn = initTurn();
 
 ////Each turn
-let nbOnGoingCards=0;
-$position1.addEventListener('click', process, $position1);
-$position2.addEventListener('click', (event) => process(event));
-//
-
 
 ////Functions
-function process(event){
-    //if (nbOnGoingCards<2) {
-    console.log(event.currentTarget.style);
-    showCard(event);
-    console.log(event.currentTarget.style);
-    setTimeout(hideCard, 1000, event);
-    //hideCard(event);
-    console.log(event.currentTarget.style);
-    //}
-    
+function process(){
+    let card=this.id; //this est l'élément HTML déclenchant
+    if (cardStatuses.get(card)==0) {
+        if (turn[0]=="empty1"){
+            showCard(this);
+            turn[0]=card;
+        } else if (turn[1]=="empty2"){
+            showCard(this);
+            turn[1]=card;
+            if (targetImages.get(turn[0])!=targetImages.get(turn[1])){
+                setTimeout(hideCard, 2000, turn[0]); //ou this
+                setTimeout(hideCard, 2500, turn[1]);
+                turn = initTurn();
+            } else {
+                console.log(turn);
+                cardStatuses.set(turn[0],1);
+                cardStatuses.set(turn[1],1);
+                turn = initTurn();
+            }
+        }
+    }
+    console.log(cardStatuses);
 }
 
-function showCard(event) {
-    //$position1.style.transform ="rotateY(180deg)"
-    event.currentTarget.style.transform ="rotateY(180deg)"
+function showCard(object) {
+    object.style.transform ="rotateY(180deg)"
 }
-function hideCard(event) {
-    //$position1.style.transform ="rotateY(0deg)"
-    event.currentTarget.style.transform ="rotateY(0deg)"
+function hideCard(id) {
+    //object.style.transform ="rotateY(0deg)"
+    document.getElementById(id).style.transform="rotateY(0deg)"
+}
+
+function initTurn() {
+    return ["empty1","empty2"];
 }
